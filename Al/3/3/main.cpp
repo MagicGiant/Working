942d7849b48075340e35dbff607e6b526d15257a
Magic <obidjanow@mail.ru>
2021-11-04 09:41:37 +0300

    first

--- Al/3/3/main.cpp
+++ Al/3/3/main.cpp
@@ -2,30 +2,15 @@
 #include <fstream>
 #include <vector>
 using namespace std;
-int n, m, k;
-bool ch=true;
-ifstream fin ("radixsirt.in.txt");
-ofstream fout("radixsort.in.txt");
-struct struct_arr
-{
-    vector <char> data;
-    struct_arr *prev;
-    struct_arr *next;
-};
-void f()
-{
-    for (int i=0; i<n; i++)
-    {
-        struct_arr *arr = new struct_arr();
-        fin >> arr.data[i];
-        if (ch)
-        {
-        }
-    }
-}
 int main()
 {
-    
+    int n, m, k;
+    ifstream fin ("radixsirt.in.txt");
+    ofstream fout("radixsort.in.txt");
     fin >> n;
+    for (int i=0; i<n; i++)
+    {
+        
+    }
     return 0;
 }
