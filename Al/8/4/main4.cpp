0a1b6ca8c4d05033d4747293d793f057947b0181
MagicGiant <obidjanow@mail.ru>
2022-02-14 23:25:42 +0300

    al lab 8 fix 4, 5

--- Al/8/4/main4.cpp
+++ Al/8/4/main4.cpp
@@ -4,22 +4,22 @@
 #include <queue>
 
 #define white 0
-#define black 1;
+#define black 1
 
 using namespace std;
 int main()
 {
-    ifstream fin ("componets.in");
-    ofstream fout ("components.out");
-    
+    ifstream fin("components.in");
+    ofstream fout("components.out");
+
     int n, m;
-    int it=1;
+    int it = 0;
     fin >> n >> m;
     int left, right;
 
     vector <queue <int>> dot(n);
-    vector <bool> color (n, white);
-    for (int i=0; i<m; i++)
+    vector <bool> color(n, white);
+    for (int i = 0; i < m; i++)
     {
         fin >> left >> right;
         left--;
@@ -28,33 +28,35 @@ int main()
         dot[right].push(left);
     }
 
-    vector <int> edge (n);
-    for (int i=0; i<n; i++)
+    vector <int> edge(n);
+    for (int i = 0; i < n; i++)
     {
-        if(color[i]==1)
+        if (color[i] == black)
             continue;
         queue <int> dot_friend;
         dot_friend.push(i);
+        it++;
 
         while (!dot_friend.empty())
         {
             int date;
-            date=dot_friend.front();
+            date = dot_friend.front();
             dot_friend.pop();
-            color[date]=black;
-            edge[date]=it++;
+            color[date] = black;
+            edge[date] = it;
             while (!dot[date].empty())
             {
-                if(color[dot[date].front()]==white)
+                if (color[dot[date].front()] == white)
                 {
                     dot_friend.push(dot[date].front());
-                    color[dot[date].front()]=black;
+                    color[dot[date].front()] = black;
                 }
                 dot[date].pop();
             }
         }
-        for (int i=0; i<n; i++)
-            fout << edge[i] << " ";
     }
+    fout << it << endl;
+    for (int i = 0; i < n; i++)
+        fout << edge[i] << " ";
     return 0;
 }
--- Al/8/5/main.cpp
+++ Al/8/5/main.cpp
@@ -7,46 +7,46 @@
 #define black 1
 
 using namespace std;
-int main ()
+int main()
 {
-    ifstream fin ("pathbge1.in");
-    ofstream fout ("pathbge1.out");
+    ifstream fin("pathbge1.in");
+    ofstream fout("pathbge1.out");
     int n, m;
     fin >> n >> m;
-    
-    vector <queue<int>> dot (n);
+
+    vector <queue<int>> dot(n);
     vector <bool> color(n, white);
     vector <int> min_path(n, 0);
 
     int left, right;
-    for (int i=0; i<n; i++)
+    for (int i = 0; i < m; i++)
     {
         fin >> left >> right;
         left--;
         right--;
         dot[left].push(right);
+        dot[right].push(left);
     }
 
     queue <int> dot_friend;
     dot_friend.push(0);
     while (!dot_friend.empty())
     {
-        int date=dot_friend.front();
+        int date = dot_friend.front();
         dot_friend.pop();
-        color[date]=black;
-        while(!dot[date].empty())
+        color[date] = black;
+        while (!dot[date].empty())
         {
-            if (color[dot[date].front()]==white)
+            if (color[dot[date].front()] == white)
             {
                 int new_date = dot[date].front();
-                dot[date].pop();
-                min_path[new_date]=min_path[date]+1;
+                min_path[new_date] = min_path[date] + 1;
                 dot_friend.push(new_date);
-                color[new_date]=black;
+                color[new_date] = black;
             }
             dot[date].pop();
         }
     }
-    for (int i=0; i<n; i++)
-        fout << min_path[i];
+    for (int i = 0; i < n; i++)
+        fout << min_path[i] << " ";
 }
